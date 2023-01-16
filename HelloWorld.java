e521cc2db53e10cdc036f597cd9f614b1618c3da
TechHog8984 <lebron.jenroy@gmail.com>
2023-01-16 14:04:06 -0500

    use bruh class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
+import bruh;
+
 public class HelloWorld {
   private static void main(String[] args) {
-    System.out.println("hello world");
+    // System.out.println("hello world");
+    bruh b = new bruh();
   }
 }
